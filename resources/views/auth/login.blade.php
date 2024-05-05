70001dabc3b7f662069e5fd5c0aff33b73afc805
Wojtyla <rapleswojtyla@gmail.com>
2024-05-06 02:21:38 +0700

    Login & Registration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,11 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
+        <!-- Username or Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="input_type" :value="__('Username/Email')" />
+            <x-text-input id="input_type" class="block mt-1 w-full" type="text" name="input_type" :value="old('input_type')" required autofocus autocomplete="input_type" />
+            <x-input-error :messages="$errors->get('input_type')" class="mt-2" />
         </div>
 
         <!-- Password -->
