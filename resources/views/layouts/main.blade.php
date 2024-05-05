e9b44efa89950175e2393b844fe4690c0381681e
Wojtyla <rapleswojtyla@gmail.com>
2024-05-05 15:50:31 +0700

    Installing Breeze and fixing something

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{$title}}</title>
+    <title>{{$title ?? config('app.name', 'laravel')}}</title>
     <!-- iconscout cdn -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
     <!-- stylesheet -->
