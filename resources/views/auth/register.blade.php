785ba715ab140530d767506bdacda8da9942eb86
JonathanChristian17 <jonathanamadeo17@gmail.com>
2024-05-18 13:58:55 +0700

    Fixed Register Page Modified

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -77,9 +77,9 @@
                 <!-- Confirm Password -->
                 <div class="input-box">
                     <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-                    <x-text-input id="password_confrimation" class="block mt-1 w-full"
+                    <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
-                            name="password_confrimation" required autocomplete="new-password" />            
+                            name="password_confirmation" required autocomplete="new-password" />            
 
                     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                     <label>Confirm Password</label>
