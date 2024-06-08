b6f54b4a561c77be4aab12d32ce58984439bebef
Wojtyla <144253449+RaplesWojtyla@users.noreply.github.com>
2024-06-08 16:21:26 +0700

    Fixed Create Post Bug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,7 +41,7 @@ class PostController extends Controller
             $tmpfile->delete();
         }
 
-        return redirect('/dashboard');
+        return back();
     }
 
     public function update(Request $request, $idPost)
